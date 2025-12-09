12ed378348bfeaa3da8322fd79ebed460846a9ab
tommaso.floris <tommaso.floris@staff.aruba.it>
2025-12-09 12:43:22 +0100

    Readd single tenant possibility to login

--- internal/config/config.go
+++ internal/config/config.go
@@ -10,30 +10,47 @@ import (
 
 // MainConfig holds operator configuration loaded from ConfigMap + Secret.
 type MainConfig struct {
-	APIGateway   string
-	KeycloakURL  string
-	VaultAddress string
-	RealmAPI     string
-	Namespace    string
-	RolePath     string
-	KVMount      string
-	RoleID       string
-	RoleSecret   string
+	APIGateway     string
+	KeycloakURL    string
+	VaultIsEnabled bool
+	VaultAddress   string
+	RealmAPI       string
+	Namespace      string
+	RolePath       string
+	KVMount        string
+	RoleID         string
+	RoleSecret     string
+	ClientID       string
+	ClientSecret   string
 }
 
 // Validate ensures all required fields are present.
 func (c *MainConfig) Validate() error {
-	required := map[string]string{
-		"api-gateway":   c.APIGateway,
-		"vault-address": c.VaultAddress,
-		"keycloak-url":  c.KeycloakURL,
-		"realm-api":     c.RealmAPI,
-		"role-path":     c.RolePath,
-		"kv-mount":      c.KVMount,
-		"role-id":       c.RoleID,
-		"role-secret":   c.RoleSecret,
-	}
 
+	var required map[string]string
+
+	if !c.VaultIsEnabled {
+		ctrl.Log.Info("Vault integration is disabled; skipping vault configuration validation")
+		required = map[string]string{
+			"api-gateway":   c.APIGateway,
+			"keycloak-url":  c.KeycloakURL,
+			"client-id":     c.RoleID,
+			"client-secret": c.RoleSecret,
+		}
+	} else {
+		ctrl.Log.Info("Vault integration is enabled; validating vault configuration also")
+
+		required = map[string]string{
+			"api-gateway":   c.APIGateway,
+			"vault-address": c.VaultAddress,
+			"keycloak-url":  c.KeycloakURL,
+			"realm-api":     c.RealmAPI,
+			"role-path":     c.RolePath,
+			"kv-mount":      c.KVMount,
+			"role-id":       c.RoleID,
+			"role-secret":   c.RoleSecret,
+		}
+	}
 	ctrl.Log.V(1).Info("Validate configurations", "required", required)
 
 	for key, val := range required {
